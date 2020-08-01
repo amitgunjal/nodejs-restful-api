f6800b4a01ccc1aad0e731c9358348c81aa1c46f
Amit Gunjal <amit.gunjal@deskera.org>
2020-08-02 02:26:50 +0530

    codacy fixes

--- app/migrations/queries.sql
+++ app/migrations/queries.sql
@@ -1,7 +1,7 @@
-CREATE TABLE "contacts" (
-  "id" int(11) NOT NULL AUTO_INCREMENT,
-  "name" varchar(255) NOT NULL,
-  "email" varchar(255) NOT NULL,
-  "active" int(11) NOT NULL,
-  PRIMARY KEY ("id")
+CREATE TABLE contacts (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  active int(11) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
